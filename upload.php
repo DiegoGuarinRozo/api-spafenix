ad3ae5709aee3329c167815d2285d73f9964287f
Daniel Santiago <danielvegarozo99@gmail.com>
2022-09-29 15:05:15 -0500

    Update upload.php

--- upload.php
+++ upload.php
@@ -1,9 +1,4 @@
 <?php 
- header('Access-Control-Allow-Origin: *');
- header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
- header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
- header("Allow: GET, POST, OPTIONS, PUT, DELETE");
-
  require_once "clases/respuestas.class.php";
  require_once "clases/conexion/conexion.php";
 
